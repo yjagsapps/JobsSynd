1d5f0aa6dbb3a59c7d051b5ecd42fcaec503fc45
yaramjags <yaramjags@gmail.com>
2014-12-02 00:24:26 -0800

    Update 1

--- app/src/main/java/com/android/yjagsapps/jobssynd/InitActivity.java
+++ app/src/main/java/com/android/yjagsapps/jobssynd/InitActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
+// This class is by Jags
 public class InitActivity extends ActionBarActivity {
 
     @Override
